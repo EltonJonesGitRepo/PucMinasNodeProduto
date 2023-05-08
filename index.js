323363ab2769420ce0743cd56bf29746a7335dbc
T0093230 <T0093230@vli-logistica.com.br>
2023-05-08 18:29:36 -0300

    Fazendo o login e a autorização funcionar

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+
+require('dotenv').config()
+
 const express = require('express');
+const morgan = require ('morgan')
+const helmet = require ('helmet')
+const bcrypt = require ('bcryptjs')
+const jwt = require ('jsonwebtoken')
+
+const routerAPIv2 = require ('./routes/routerAPI-v2')
+//app.use ('/api/v2', routerAPIv2)
+
 const bodyParser = require('body-parser');
 const knex = require('knex')({
     client: 'sqlite3',
@@ -9,6 +20,41 @@ const knex = require('knex')({
 
 const app = express();
 app.use(bodyParser.json());
+
+
+app.use (morgan("tiny"))
+app.use (helmet())
+
+// app.use (function (req, res) {
+//     res.status(404).send ('Recurso não encontrado.')
+//   })
+
+
+const checkToken = (req, res, next) => {
+    let authInfo = req.get('authorization')
+    console.log(authInfo);
+    if (authInfo) {
+      const [bearer, token] = authInfo.split(' ')
+      
+      if (!/Bearer/.test(bearer)) {
+        res.status(400).json({ message: 'Tipo de token esperado não informado...', error: true })
+        return 
+      }
+  
+      jwt.verify(token, process.env.SECRET_KEY, (err, decodeToken) => {
+          if (err) {
+              res.status(401).json({ message: 'Acesso negado'})
+              return
+          }
+          req.usuarioId = decodeToken.id
+          req.roles = decodeToken.roles
+          next()
+      })
+    } 
+    else
+      res.status(401).json({ message: 'Acesso negado'})
+  }
+
 
 app.get('/carros', async (req, res) => {
     try {
@@ -86,6 +132,72 @@ app.delete('/carros/:id', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('API rodando na porta 3000');
+// Cria um manipulador da rota padrão 
+app.get('/produtos', checkToken, function (req, res) {
+    //app.get('/produtos', function (req, res) {
+    knex.select('*').from('produtos')
+    .then (produtos => res.json(produtos))
+    .catch (err => res.json ({ message: `Erro ao recuperar produtos: ${err.message}` }))
+  });
+
+
+
+  app.post('/seguranca/register', function (req, res) {
+    knex('usuarios').insert({
+          nome: req.body.nome, 
+          login: req.body.login, 
+          senha: bcrypt.hashSync(req.body.senha, 8), 
+          email: req.body.email,
+          roles: "USER"
+      }, ['id'])
+      .then((result) => {
+          let usuario = result[0]
+          res.status(200).json({
+            "message": "Usuário inserido com sucesso",
+            "id": usuario.id }) 
+          return
+      })
+      .catch(err => {
+          res.status(500).json({ 
+              message: 'Erro ao registrar usuario - ' + err.message })
+      })  
+  });
+
+
+  app.post('/seguranca/login', function (req, res) {
+  knex
+  .select('*').from('usuarios').where( { login: req.body.login })
+  .then( usuarios => {
+      if(usuarios.length){
+          let usuario = usuarios[0]
+          let checkSenha = bcrypt.compareSync (req.body.senha, usuario.senha)
+          if (checkSenha) {
+             var tokenJWT = jwt.sign({ id: usuario.id, roles: usuario.roles }, 
+                  process.env.SECRET_KEY, {
+                    expiresIn: 3600
+                  })
+
+              res.status(200).json ({
+                  id: usuario.id,
+                  login: usuario.login, 
+                  nome: usuario.nome, 
+                  roles: usuario.roles,
+                  token: tokenJWT
+              })  
+              return 
+          }
+      } 
+        
+      res.status(401).json({ message: 'Login ou senha incorretos' })
+  })
+  .catch (err => {
+      res.status(500).json({ 
+         message: 'Erro ao verificar login - ' + err.message })
+  })
+
+});
+  
+
+app.listen(3001, () => {
+    console.log('API rodando na porta 3001');
 });
